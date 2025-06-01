57886a5f651691a78200a5451dfb2a00d9c72446
Abdirazakf <farahabdirazak13@gmail.com>
2025-06-01 19:32:55 -0400

    Finalized the etch a sketch board design and added functionality to the screen so when the mouse hovers over a square it changes the color to black

--- scripts.js
+++ scripts.js
@@ -3,8 +3,11 @@ const container = document.querySelector("#container")
 for (let i = 1; i <= 256; i++){
     const item = document.createElement("div")
     item.classList.add("item", "item-" + i)
+    // item.textContent = i
     container.appendChild(item)
-}
 
-const buttons = document.querySelectorAll("button")
+    item.addEventListener("mouseover", () => {
+        item.setAttribute("style", "background-color: black; border: 3px solid black")
+    })
+}
 
